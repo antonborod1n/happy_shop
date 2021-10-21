29a4280c6c9820251850795754148834c106309e
antonborod1n <antonborodin86@gmail.com>
2021-10-21 13:51:53 +0300

    added benefits

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $ (function(){
-
+    $('.top-slider__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icons-slider/arrows-left.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icons-slider/arrows-right.svg" alt="arrow"></button>',
+    });
 });
