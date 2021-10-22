bcd139d8b9ff765af63b4dc59100c09fa9638ace
antonborod1n <antonborodin86@gmail.com>
2021-10-23 00:27:57 +0300

    added product, no slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,10 @@ $ (function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icons-slider/arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icons-slider/arrows-right.svg" alt="arrow"></button>',
     });
+    $(".star").rateYo({
+        starWidth: "15px",
+        normalFill: "#ededed",
+        ratedFill: "#ffcc00",
+        readOnly: true
+      });
 });
