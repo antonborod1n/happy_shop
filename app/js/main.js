bc811268b120d1320bcf51662868a76763947436
antonborod1n <antonborodin86@gmail.com>
2021-10-23 22:38:37 +0300

    added popular product

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 $ (function(){
-    /* $('.top-slider__slider').slick({
+    $('.top-slider__slider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icons-slider/arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icons-slider/arrows-right.svg" alt="arrow"></button>',
-    }); */
+    });
     $('.slider_slider').slick({
         infinite: true,
         speed: 300,
