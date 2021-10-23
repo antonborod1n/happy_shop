7f1e5d4d52763e1046625b6a19efc0bd6f236cc0
antonborod1n <antonborodin86@gmail.com>
2021-10-23 10:50:27 +0300

    added partners

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 $ (function(){
-    $('.top-slider__slider').slick({
+    /* $('.top-slider__slider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icons-slider/arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icons-slider/arrows-right.svg" alt="arrow"></button>',
+    }); */
+    $('.slider_slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
     $(".star").rateYo({
         starWidth: "15px",
