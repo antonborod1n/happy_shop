99a9c859f5b451d114e14ea6a77bc1314b278074
antonborod1n <antonborodin86@gmail.com>
2021-12-01 08:32:04 +0300

    added slise catalog

--- app/js/main.js
+++ app/js/main.js
@@ -6,16 +6,26 @@ $(function () {
         $('.popular__tabs').removeClass('popular__tabs--active');
         $($(this).attr('href')).addClass('popular__tabs--active');
     });
+
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
     });
+
     $('.menu__catalog-btn').on('click', function () {
         $('.top-slider__menu').toggleClass('top-slider__menu--active');
     });
+
+    $('.product-item').slice(0, 15).show();
+    $('.shop-content__bottom-btn').on('click', function (k) {
+        k.preventDefault();
+        $('.product-item:hidden').slice(0, 15).slideDown();
+    });
+
     $('.top-slider__slider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icons-slider/arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icons-slider/arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.slider_slider').slick({
         infinite: true,
         speed: 300,
@@ -25,6 +35,7 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.goods-slider').slick({
         infinite: true,
         speed: 300,
@@ -34,6 +45,7 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.slider_slider__mini').slick({
         infinite: true,
         speed: 300,
@@ -43,6 +55,7 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.slider__one').slick({
         infinite: true,
         speed: 300,
@@ -52,6 +65,7 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.blog_slider').slick({
         infinite: true,
         speed: 300,
@@ -61,6 +75,7 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.goods-content__slide-thumb').slick({
         asNavFor: '.goods-content__slide-big',
         focusOnSelect: true,
@@ -71,18 +86,21 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/product/icons/product-arrows-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/product/icons/product-arrows-right.svg" alt="arrow"></button>',
     });
+
     $('.goods-content__slide-big').slick({
         asNavFor: '.goods-content__slide-thumb',
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
     });
+
     $(".star").rateYo({
         starWidth: "15px",
         normalFill: "#ededed",
         ratedFill: "#ffcc00",
         readOnly: true,
     });
+
     $(".star-big").rateYo({
         starWidth: "25px",
         normalFill: "#ededed",
